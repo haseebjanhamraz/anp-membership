374b6aa3eeb82e855b86d1bddb5ab197e7375abf
haseebjanhamraz <haseebjanhamraz@gmail.com>
2024-02-21 00:53:22 +0500

    Changed loading animation

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -4,8 +4,11 @@ import logo from "../img/anp-logo.png"; // Import your logo
 const Spinner = () => {
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-sky-600"></div>
+      <div className="absolute animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-red-600"></div>
       <img src={logo} alt="Logo" className="absolute w-24 h-24" />
+      <div className="flex justify-center mt-44">
+        <h1 className="text-3xl text-red-400 font-bold">Loading...</h1>
+      </div>
     </div>
   );
 };
